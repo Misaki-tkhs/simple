9024886ef9682c7f6d6b4bcfd7a8a2c05f390195
Misaki-tkhs <misaki.takahashi@force-corp.co.jp>
2021-04-26 13:11:12 +0900

    test

--- js/if-for.js
+++ js/if-for.js
@@ -1,4 +1,4 @@
-'use strict'
+// 'use strict'
 
 // let i = 2;
 // if (i === 1){
@@ -7,12 +7,13 @@
 
 // let testScore = 80;
 
-function scoreCheck() {
-    let testScore = document.getElementById('score').value
-    if (testScore >= 80){
-        document.write("合格です！");
-    } else if (testScore >= 70) {
-        document.write("惜しい！");
-    } else {
-        document.write("不合格です！");}
-}
+// function scoreCheck() {
+//     let testScore = document.getElementById('score').value
+//     if (testScore >= 80){
+//         document.write("合格です！");
+//     } else if (testScore >= 70) {
+//         document.write("惜しい！");
+//     } else {
+//         document.write("不合格です！");}
+// }
+
